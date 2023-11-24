2ef432a6f857d451b58d60ffb586efaa4b8cf20c
Daniel Hendriks <567492@edu.rocmn.nl>
2023-11-24 09:33:12 +0100

    add database connection

---
+++ private/config.php
@@ -0,0 +1,5 @@
+<?php 
+
+require_once("../static/database.php")
+
+?>
--- static/database.php
+++ static/database.php
@@ -1,4 +1,6 @@
 <?php 
 
+$con = new PDO("mysql:host=portfolio.ictcampus.nl;dbname=top2000_1", 'dbuser_top2000_1', 'C&b64s#aVs');
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
