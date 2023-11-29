f8ea00b9f6cff142cfedd14b5e576e8235b1da06
Daniel Hendriks <567492@edu.rocmn.nl>
2023-11-29 10:25:45 +0100

    add head and navbar page

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php 
 
-require_once("../private/config.php")
+require_once("../private/config.php");
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once "../private/components/head.php" ?>
+</head>
+<body>
+    <?php require_once "../private/components/navbar.php" ?>
+    
+</body>
+</html>
