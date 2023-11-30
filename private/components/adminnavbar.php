af72439555d668dbd13c860ce10c4bb350dd7e34
Tim Swildens <567203@edu.rocmn.nl>
2023-11-30 11:53:15 +0100

    admin navbar gemaakt

---
+++ private/components/adminnavbar.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="navbarstyle.css">
+    <title>Document</title>
+    <style>
+        .navbar{
+            background-color:  #DA151A !important;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+<img src="img/npo_radio2_top2000_logo.svg" alt="Girl in a jacket" width="100" height="60">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+    <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Dj</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Nummer/artiest</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Stemmers</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Gebruiker</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Qr scanner</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">PDF</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
