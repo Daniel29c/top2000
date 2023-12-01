dcff0f7f426a43c6ac681d67c456798772f0da34
Tim Swildens <567203@edu.rocmn.nl>
2023-12-01 09:09:24 +0100

    autoloader

---
+++ private/autoloader.php
@@ -0,0 +1,19 @@
+<?php
+require_once "../static/database.php";
+require_once "config.php";
+
+session_start();
+
+spl_autoload_register(function ($className) {
+    $baseNamespace = 'managers';
+
+    $classFile = str_replace($baseNamespace, '', $className);
+    $classFile = str_replace('\\', '/', $classFile);
+
+    $filePath = 'managers/' . $classFile . '.php';
+
+    require_once $filePath;
+
+});
+
+?>
