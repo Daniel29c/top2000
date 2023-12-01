bd36d629e83e9eb98dac74d11a13c9dc8e2b7b76
Tim Swildens <567203@edu.rocmn.nl>
2023-12-01 08:43:35 +0100

    bootstrap icon toegevoegd

--- private/components/navbar.php
+++ private/components/navbar.php
@@ -28,6 +28,9 @@
       <li class="nav-item active">
         <a class="nav-link" href="#">Programmering</a>
       </li>
+      <li class="nav-item active">
+        <i class="bi bi-person-circle"></i>
+      </li>
     </ul>
   </div>
 </nav>
