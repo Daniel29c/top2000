1fba2a1daae58a6666b464969967dcfca47dab8f
Tim Swildens <567203@edu.rocmn.nl>
2023-12-05 11:36:18 +0100

    uitslag

---
+++ public/staatie_uitslag.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("../private/config.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once "../private/components/head.php" ?>
+    <style>
+    body {
+        background-color: #22282E;
+    }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
