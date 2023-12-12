ce94d898313692160e1c4389a065a102fead3546
Tim Swildens <567203@edu.rocmn.nl>
2023-12-12 09:09:20 +0100

    functie

--- public/staatie_uitslag.php
+++ public/staatie_uitslag.php
@@ -1,6 +1,14 @@
 <?php
-
 require_once("../private/config.php");
+require_once "../private/managers/staatieManager.php";
+
+// $song = StaatieManager::searchSong($name, $artist);
+
+// if ($song->num_rows > 0) {
+//     echo "<h2>Song Found: Yes</h2>";
+// } else {
+//     echo "<h2>Song Found: No</h2>";
+// }
 
 ?>
 <!DOCTYPE html>
