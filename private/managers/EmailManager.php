ffb4b0ee030dec7ab2d62c9422c67be83fc5d151
Tim Swildens <567203@edu.rocmn.nl>
2023-12-20 14:26:43 +0100

    php mailer

--- private/managers/EmailManager.php
+++ private/managers/EmailManager.php
@@ -1,57 +1,48 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
-require '../static/PHPMailer/Exception.php';
-//require 'PHPMailer/Exception.php';
-require '../static/PHPMailer/PHPMailer.php';
-require '../static/PHPMailer/SMTP.php';
-//include '../static/config.php';
-//require '../autoloader.php';
+use PHPMailer\PHPMailer\Exception;
 
+require_once "../static/settings.php";
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-
-class EmailManager {
-    public static function sendEmail($id ,$HTML){
-        $ticket = CafeManager::c
-        $customer = CustomerManager::getCustomerById($id);
-        //var_dump($id);
-        $config = config::settings();
+class EmailManager
+{
+    public static function send($id, $html){
+        $customer = UserManager::getUserById($id);
+        $settings = config::settings();
         $mail = new PHPMailer(true);
+
         try {
             //Server settings
-            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+           // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = $config["Host"];                    //Set the SMTP server to send through
+            $mail->Host       = $settings['Host'];                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = $config["Username"];                    //SMTP username
-            $mail->Password   = $config["Password"];                              //SMTP password
+            $mail->Username   = $settings['Username'];                   //SMTP username
+            $mail->Password   = $settings['password'];                            //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $mail->Port       = $settings['port'];                                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
             //Recipients
-            $mail->setFrom('student@ictcampus.nl', 'Mailer');
-            $mail->addAddress($customer->email);     //Add a recipient
-            // $mail->addAddress('ellen@example.com');               //Name is optional
-            // $mail->addReplyTo('info@example.com', 'Information');
-            // $mail->addCC('cc@example.com');
-            // $mail->addBCC('bcc@example.com');
-
+            $mail->setFrom('student@ictcampus.nl', 'Top2000');
+            $mail->addAddress($customer['email'], (ucfirst($customer['firstname']) . " " . $customer['lastname'])); 
+        
             //Content
             $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = "Jouw games";
-            $mail->Body    = $HTML;
-            // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->Subject = 'Testmail';
+            $mail->Body    = $html;
+            $mail->AltBody = 'Kan geen mail sturen';
         
             $mail->send();
-            
-            return true;
+            echo 'Message has been sent';
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-            return false;
-        }
+                }   
+            }
         }
 
-    }
-
-?>
