97db4fa30411b3fac316226fe06c270322cb423d
Daniel Hendriks <567492@edu.rocmn.nl>
2024-01-06 21:37:46 +0100

    add news page

--- private/components/navbar.php
+++ private/components/navbar.php
@@ -25,18 +25,19 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-light">
-        <img src="img/npo_radio2_top2000_logo.svg" width="100" height="60">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
-            aria-expanded="false" aria-label="Toggle navigation">
+        <a href="index.php">
+            <img src="img/npo_radio2_top2000_logo.svg" width="100" height="60">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav justify-content-center">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Nieuws</a>
+                    <a class="nav-link" href="nieuws.php">Nieuws</a>
                 </li>
                 <li class="nav-item active">
                     <a class="nav-link" href="programmering.php">Programmering</a>
---
+++ public/nieuws.php
@@ -0,0 +1,77 @@
+<?php 
+
+require_once("../private/config.php");
+
+$articles = newsarticleManager::getAllNews();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once "../private/components/head.php"; ?>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .news-container {
+            max-width: 80%;
+            margin: auto;
+        }
+        .news-item {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 20px;
+            justify-content: space-between;
+        }
+        .news-article {
+            width: calc(33.33% - 20px);
+            box-sizing: border-box;
+        }
+        .news-title {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+        .news-content {
+            color: #555;
+        }
+        .news-image {
+            max-width: 100%;
+            height: auto;
+            margin-bottom: 10px;
+        }
+        /* Style for the anchor tag */
+        .news-article a {
+            text-decoration: none; /* Remove underline */
+            color: inherit; /* Use the default color (usually black) */
+        }
+    </style>
+</head>
+<body>
+<?php require_once "../private/components/navbar.php" ?>
+
+<div class="news-container">
+    <h1>Laatste nieuws</h1>
+
+    <?php
+    echo '<div class="news-item">';
+
+    // Loop through the news articles and display them using foreach
+    foreach ($articles as $article) {
+        echo '<div class="news-article">';
+        echo '<a href="artikel.php?id=' . $article->id . '">';
+        echo '<img class="news-image" src="' . $article->imgpath . '" alt="">';
+        echo '<div class="news-title">' . $article->Titel . '</div>';
+        echo '<div class="news-content">' . $article->author . '</div>';
+        echo '</a>';
+        echo '</div>';
+    }
+
+    // Close the news items container
+    echo '</div>';
+    ?>
+</div>
+
+</body>
+</html>
